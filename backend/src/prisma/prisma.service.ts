9f1f2949a13157cbf86e1b81672708ae55a10061
andre.kortkamp <andre.kortkamp@genezys.io>
2026-05-30 20:21:04 -0300

    feat: exclude prisma folder from tsconfig.build.json to preserve dist structure

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,16 +1,12 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
-  constructor() {
-    const adapter = new PrismaPg(process.env['DATABASE_URL']!);
-    super({ adapter });
-  }
+  implements OnModuleInit, OnModuleDestroy {
+  // O construtor com adapter foi removido. 
+  // O PrismaClient nativo vai ler a DATABASE_URL do Render automaticamente.
 
   async onModuleInit() {
     await this.$connect();
